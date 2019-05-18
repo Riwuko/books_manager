01069f9a2a93f61e7efd8105a7707f219924c952
Dawid Żurawski <dawid.zurawski@protonmail.com>
2019-05-19 00:04:16 +0200

    Changes in main.py - apply configs.

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from flask import Flask
 
+from config import APP, DB
 from endpoints import endpoint
 from errors import error
 from models import db
@@ -9,8 +10,10 @@ app = Flask(__name__)
 app.register_blueprint(endpoint)
 app.register_blueprint(error)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///foo.db'
+app.config[
+    'SQLALCHEMY_DATABASE_URI'
+] = f'postgresql+psycopg2://{DB.USER}:{DB.PASSWORD}@{DB.HOST}:{DB.PORT}/{DB.DBNAME}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'abc'
+app.config['SECRET_KEY'] = APP.SECRET_KEY
 
 db.init_app(app)
