a89b760cdd3ae5387300de9fc72168e0e3744022
Dawid Żurawski <dawid.zurawski@protonmail.com>
2019-05-19 12:45:39 +0200

    Tested prepare_new_books function.

--- unittests/test_tools.py
+++ unittests/test_tools.py
@@ -2,7 +2,7 @@ from unittest.mock import Mock, patch
 
 import pytest
 
-from app.tools import import_books
+from app.tools import import_books, prepare_new_books
 
 
 class GoogleResponse:
@@ -75,3 +75,98 @@ class GoogleResponse:
 def test_import_books(response, output):
     with patch('tools.requests.get', Mock(return_value=GoogleResponse(response))):
         assert import_books('keyword') == output
+
+
+class ExistingBook:
+    def __init__(self, author, title):
+        self.author = author
+        self.title = title
+
+
+@pytest.mark.parametrize(
+    'imported_books, existing_books_params, expected_output',
+    (
+        (
+            [
+                {
+                    'author': 'John, Mike',
+                    'title': 'something',
+                    'category': 'fantasy, horror',
+                    'description': 'some description',
+                },
+                {
+                    'author': 'Amy',
+                    'title': 'something else',
+                    'category': 'fiction',
+                    'description': 'space around the earth',
+                },
+            ],
+            [('Amy', 'something else'), ('John, Mike', 'bbb'), ('Someone', 'something')],
+            [
+                {
+                    'author': 'John, Mike',
+                    'title': 'something',
+                    'category': 'fantasy, horror',
+                    'description': 'some description',
+                }
+            ],
+        ),
+        (
+            [
+                {
+                    'author': 'John, Mike',
+                    'title': 'something',
+                    'category': 'fantasy, horror',
+                    'description': 'some description',
+                },
+                {
+                    'author': 'Amy',
+                    'title': 'something else',
+                    'category': 'fiction',
+                    'description': 'space around the earth',
+                },
+            ],
+            [('Amy', 'something else'), ('John, Mike', 'bbb'), ('Someone', 'something'), ('John, Mike', 'something')],
+            [],
+        ),
+        (
+            [
+                {
+                    'author': 'John, Mike',
+                    'title': 'something',
+                    'category': 'fantasy, horror',
+                    'description': 'some description',
+                },
+                {
+                    'author': 'Amy',
+                    'title': 'something else',
+                    'category': 'fiction',
+                    'description': 'space around the earth',
+                },
+            ],
+            [
+                ('Amy', 'something else2'),
+                ('John, Mike', 'bbb'),
+                ('Someone', 'something'),
+                ('John, Mike', 'something22'),
+            ],
+            [
+                {
+                    'author': 'John, Mike',
+                    'title': 'something',
+                    'category': 'fantasy, horror',
+                    'description': 'some description',
+                },
+                {
+                    'author': 'Amy',
+                    'title': 'something else',
+                    'category': 'fiction',
+                    'description': 'space around the earth',
+                },
+            ],
+        ),
+    ),
+)
+def test_prepare_new_books(imported_books, existing_books_params, expected_output):
+    existing_books = [ExistingBook(*params) for params in existing_books_params]
+    assert prepare_new_books(imported_books, existing_books) == expected_output
